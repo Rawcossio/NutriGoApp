8e98b8e3b8445467ca67b0b4c39978fdfd39731a
Rawcossio <raulcossio166183@gmail.com>
2025-02-27 11:13:53 -0500

    se crea proyecto base

---
+++ src/main/java/com/example/NutriGoApp/modelos/Usuario.java
@@ -0,0 +1,4 @@
+package com.example.NutriGoApp.modelos;
+
+public class Usuario {
+}
---
+++ src/test/java/com/example/NutriGoApp/NutriGoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NutriGoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriGoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
