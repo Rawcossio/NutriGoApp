2efa25d57efe17edc78d8f827855cbe5b32e9cd6
Rawcossio <raulcossio166183@gmail.com>
2025-03-06 17:32:03 -0500

    se agregan enums y se crean las clases detalle, direccion y entrega que estaban pendientes de convertir en tablas

--- src/main/java/com/example/NutriGoApp/modelos/Entrega.java
+++ src/main/java/com/example/NutriGoApp/modelos/Entrega.java
@@ -1,6 +1,52 @@
 package com.example.NutriGoApp.modelos;
 
+import com.example.NutriGoApp.ayudas.enums.EntregaEstado;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="Entregas")
 public class Entrega {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_entrega")
+    private Integer id_entrega;
+    @Column(name="fecha_entrega", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime fecha_de_entrega;
+    @Column(name="estado_entrega", columnDefinition = "VARCHAR(10) DEFAULT 'ASIGANDO' ")
+    private EntregaEstado estado_entrega;
 
+    public Entrega() {
+    }
 
+    public Entrega(Integer id_entrega, LocalDateTime fecha_de_entrega, EntregaEstado estado_entrega) {
+        this.id_entrega = id_entrega;
+        this.fecha_de_entrega = fecha_de_entrega;
+        this.estado_entrega = estado_entrega;
+    }
+
+    public Integer getId_entrega() {
+        return id_entrega;
+    }
+
+    public void setId_entrega(Integer id_entrega) {
+        this.id_entrega = id_entrega;
+    }
+
+    public LocalDateTime getFecha_de_entrega() {
+        return fecha_de_entrega;
+    }
+
+    public void setFecha_de_entrega(LocalDateTime fecha_de_entrega) {
+        this.fecha_de_entrega = fecha_de_entrega;
+    }
+
+    public EntregaEstado getEstado_entrega() {
+        return estado_entrega;
+    }
+
+    public void setEstado_entrega(EntregaEstado estado_entrega) {
+        this.estado_entrega = estado_entrega;
+    }
 }
